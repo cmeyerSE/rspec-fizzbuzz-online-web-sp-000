89c16d6dc71db0e95b350ee31941fd1d4f4dd01c
Cory Meyer <cameyer182@gmail.com>
2021-06-17 03:06:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
-
-    fizzbuzz(3).to eq("Fizz")
-  end
-
-  if int % 5 == 0
-
-    fizzbuzz(5).to eq("Buzz")
-  end
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif int % 3 & 5 == 0
+    "FizzBuzz"
+    end
 end
