cb49d85c55e30687dbeb8993b530d81770fae537
Cory Meyer <cameyer182@gmail.com>
2021-06-17 03:54:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,22 +1,13 @@
 def fizzbuzz(int)
   if int % 3 == 0
-
-    int = 3
-
     "Fizz"
   end
 
   if int % 5 == 0
-
-    int = 5
-
     "Buzz"
   end
 
-  if int % 3 && 5 == 0
-
-    int = 15
-
+  if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   end
 end
